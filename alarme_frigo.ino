e932b9e0de01223daa4e225b6b3e0ee7692da1b5
Siham <46928466+siham87@users.noreply.github.com>
2019-01-29 14:40:00 +0100

    Add files via upload

---
+++ alarme_frigo.ino
@@ -0,0 +1,29 @@
+void setup() {
+
+Serial.begin(9600);
+pinMode(8,OUTPUT);
+}
+
+void loop() {
+  
+  int lumiere = analogRead(0);
+
+  Serial.println("lumiere : ");
+  Serial.println(lumiere);
+  delay(1000);
+  digitalWrite(8,LOW);
+  
+  if(lumiere > 300){
+
+    int lumiere = analogRead(0);
+    delay(5000);
+    lumiere = analogRead(0);
+  
+      if(lumiere > 300){
+      Serial.println("alarm");
+      digitalWrite(8,HIGH);
+      lumiere = analogRead(0);
+      }
+  
+}
+}
